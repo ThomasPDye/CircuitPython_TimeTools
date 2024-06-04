7564fc2da7ef0ea121ab78f14ac116799ce8d5a4
ThomasPDye <tompdye@googlemail.com>
2024-06-04 09:52:05 +0100

    first functional commit of timetools

--- timetools.py
+++ timetools.py
@@ -24,14 +24,21 @@ Implementation Notes
 * Adafruit CircuitPython firmware for the supported boards:
   https://circuitpython.org/downloads
 
-.. todo:: Uncomment or remove the Bus Device and/or the Register library dependencies
-  based on the library's use of either.
-
-# * Adafruit's Bus Device library: https://github.com/adafruit/Adafruit_CircuitPython_BusDevice
-# * Adafruit's Register library: https://github.com/adafruit/Adafruit_CircuitPython_Register
+* Adafruit's NTP library: https://github.com/adafruit/Adafruit_CircuitPython_NTP
 """
 
 # imports
 
+import time
+
 __version__ = "0.0.0+auto.0"
 __repo__ = "https://github.com/ThomasPDye/CircuitPython_TimeTools.git"
+
+
+def struct_time_equivalent(time_a: time.struct_time, time_b: time.struct_time):
+    """compare time_a and time_b for equivalence taking into account unknowns"""
+    for i in range(len(time.struct_time)):
+        member_a, member_b = time_a[i], time_b[i]
+        if member_a != member_b and (member_a != -1 or member_b != -1):
+            return False
+    return True
